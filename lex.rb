05bf59acbf53e312983105c554a5945d7e897c5e
Andy Gauge <andygauge@gmail.com>
2016-03-30 16:05:02 -0700

    Uses bible-api.com to return verse

--- lex.rb
+++ lex.rb
@@ -1,2 +1,6 @@
 require './lib/importer'
-puts Importer.lex ARGV[0]
+require 'net/http'
+require 'uri'
+require 'json'
+response = JSON.parse(Net::HTTP.get_response(URI.parse(URI.escape("http://bible-api.com/#{Importer.lex ARGV[0]}"))).body)
+puts "#{response["reference"]} #{response["text"]}"
